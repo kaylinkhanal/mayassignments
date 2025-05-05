57deca27bd7478d5191ea76247817410d40a28ef
bishal bomjan <bishalbomjan747@gmail.com>
2025-05-05 09:17:51 +0545

    added

--- arrays/level 1.js
+++ arrays/level 1.js
@@ -5,7 +5,13 @@ const splitedArrau = arr.slice(' ').join('-')
 console.log(splitedArrau.toUpperCase())
 
 //Q2 output should be ['ram','thapa']
+
+//By splice method
 const newOutput = arr.slice(' ')
-const output = [newOutput[0], newOutput[2]]
-console.log(output);
+newOutput.splice(1, 1)
+console.log(newOutput);
+
+//by another method
+// const output = [newOutput[0], newOutput[2]]
+// console.log(output);
 
