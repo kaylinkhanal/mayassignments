32b4c15d4179c9c0d7cd0fa5b0062a5ec5ee346f
bishal bomjan <bishalbomjan747@gmail.com>
2025-05-05 09:02:29 +0545

    level 1 question solved

--- arrays/level 1.js
+++ arrays/level 1.js
@@ -1,6 +1,11 @@
 const arr = ['ram', 'bahadur', 'thapa']
 
 //Q1 output should be 'RAM-BAHADUR-THAPA'
-
+const splitedArrau = arr.slice(' ').join('-')
+console.log(splitedArrau.toUpperCase())
 
 //Q2 output should be ['ram','thapa']
+const newOutput = arr.slice(' ')
+const output = [newOutput[0], newOutput[2]]
+console.log(output);
+
