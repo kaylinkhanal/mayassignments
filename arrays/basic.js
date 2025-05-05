ea709c7a1df85093a1470f765163307c039a30a8
pratham karki <prathamkarki21@gmail.com>
2025-05-05 08:57:05 +0545

    array lvl 1 file added

--- arrays/basic.js
+++ arrays/basic.js
@@ -1,10 +1,7 @@
 const arr = [1,4,5,6,7,1]
 
 // Q1 output should be 6
-<<<<<<< HEAD
 console.log(arr.length);
-=======
->>>>>>> fd605ddb883d30c48be3328655cabe16a79bf11e
 
 // Q2 output should be [1,4,5]
 console.log(arr.slice(0,3))
