1c4e3914ea202bf0ca480c4e4ca248f50dd5c973
pratham karki <prathamkarki21@gmail.com>
2025-05-05 09:01:24 +0545

    level 1 js problem solved

--- arrays/level 1.js
+++ arrays/level 1.js
@@ -1,6 +1,9 @@
 const arr = ['ram', 'bahadur', 'thapa']
 
 //Q1 output should be 'RAM-BAHADUR-THAPA'
-
+const joined = arr.join('-').toUpperCase();
+console.log(joined);
 
 //Q2 output should be ['ram','thapa']
+arr.splice(1,1);
+console.log(arr);
