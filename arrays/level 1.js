e0e6d8372aeb82183cb1edf91f3592fd51dd2ca2
bishal bomjan <bishalbomjan747@gmail.com>
2025-05-05 09:19:03 +0545

    removed slice

--- arrays/level 1.js
+++ arrays/level 1.js
@@ -1,13 +1,13 @@
 const arr = ['ram', 'bahadur', 'thapa']
 
 //Q1 output should be 'RAM-BAHADUR-THAPA'
-const splitedArrau = arr.slice(' ').join('-')
+const splitedArrau = arr.join('-')
 console.log(splitedArrau.toUpperCase())
 
 //Q2 output should be ['ram','thapa']
 
 //By splice method
-const newOutput = arr.slice(' ')
+
 newOutput.splice(1, 1)
 console.log(newOutput);
 
