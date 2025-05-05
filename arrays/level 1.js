24ef2900e7dc675580d8aab709720816a45dbbab
manoj <admanoj111@gmail.com>
2025-05-05 09:21:36 +0545

    add level

--- arrays/level 1.js
+++ arrays/level 1.js
@@ -1,6 +1,8 @@
-const arr = ['ram', 'bahadur', 'thapa']
+const arr = ["ram", "bahadur", "thapa"];
 
 //Q1 output should be 'RAM-BAHADUR-THAPA'
-
+console.log(arr.join("-").toUpperCase());
 
 //Q2 output should be ['ram','thapa']
+result1 = arr.splice(1, 1);
+console.log(arr);
