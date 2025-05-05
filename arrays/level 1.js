24801838ce70c55253fa8834e101d2d8e9cd8725
samit singh bagchan <samitsbagchan@gmail.com>
2025-05-05 09:13:06 +0545

    array solved

--- arrays/level 1.js
+++ arrays/level 1.js
@@ -1,6 +1,9 @@
 const arr = ['ram', 'bahadur', 'thapa']
 
 //Q1 output should be 'RAM-BAHADUR-THAPA'
+console.log(arr.join('-').toUpperCase());
+
 
 
 //Q2 output should be ['ram','thapa']
+console.log(arr.splice(1,1));
