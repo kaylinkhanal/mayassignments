7c33d0ee6b825114effaab4339b04b33be500fdd
Kaylin Khanal <kaylinkhanal@Kaylins-MacBook-Pro-2.local>
2025-05-08 08:44:59 +0545

    added additional question for object basic

--- objects/basic.js
+++ objects/basic.js
@@ -4,13 +4,22 @@
 // object is based on keys for retrieval (using dot operator)
 
 let scores =  {
-    kaylin: 350,
-    ram: 503,
-    shyam: 701
+    math: 350,
+    science: 503,
+    computer: 701
 }
 
 
-console.log(scores.kaylin)
+console.log(scores.science)
 
 
 //Q1. calculate the percetange scored, full marks of each subject is 1000, and round off to 2 decimal places
+//Q2. what is destructuring an object. solve Q1 by destructuring method.
+const {math, science, a} = scores;
+console.log((math+science+computer)/3000*100)
+
+// Q3. delete science from scores object
+//expected output: { math: 350, computer: 701 }
+
+// Q4. add a new subject called 'english' with score 800
+//expected output: { math: 350, science: 503, computer: 701}
