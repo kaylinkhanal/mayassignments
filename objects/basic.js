63bd1f7c7c74a5b32ccbf48a711e6243a6b60a6d
Laxmi-999 <regmilaxmi325@gmail.com>
2025-05-08 08:46:02 +0545

    qn 1 and 2 solved

--- objects/basic.js
+++ objects/basic.js
@@ -10,9 +10,23 @@ let scores =  {
 }
 
 // Calculate percentage scored and round off to 2 decimal places
-let fullMarks = 1000;
+let fullMarks = 3000;
+let totalScore = Object.values(scores).reduce((sum, value) => sum + value, 0);
 
-for (let person in scores) {
-    let percentage = (scores[person] / fullMarks) * 100;
-    console.log(`${person}: ${percentage.toFixed(2)}%`);
-}
+
+
+console.log(`Total Score: ${totalScore}`);
+
+    let percentage = (totalScore/ fullMarks) * 100;
+    console.log( percentage.toFixed(2));
+
+// q2 --> what is destructing of an object. solve it by destructing method
+
+const {math, science, computer} = scores;
+
+
+const percentage2 = ((math+science+computer)/fullMarks)*100;
+
+
+console.log("percentage2 of score is", percentage2.toFixed(2) );
+
