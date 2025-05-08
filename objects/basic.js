726c1e76a3164bc0309d8d6ca5867c27899bcc07
Laxmi-999 <regmilaxmi325@gmail.com>
2025-05-08 08:53:46 +0545

    qn3 and 4 solved

--- objects/basic.js
+++ objects/basic.js
@@ -12,8 +12,7 @@ let scores =  {
 // Calculate percentage scored and round off to 2 decimal places
 let fullMarks = 3000;
 let totalScore = Object.values(scores).reduce((sum, value) => sum + value, 0);
-
-
+// let totalScore = scores.computer+scores.math+scores.science;
 
 console.log(`Total Score: ${totalScore}`);
 
@@ -30,3 +29,37 @@ const percentage2 = ((math+science+computer)/fullMarks)*100;
 
 console.log("percentage2 of score is", percentage2.toFixed(2) );
 
+
+
+// Q3. delete science from scores object
+//expected output: { math: 350, computer: 701 }
+
+// Q3. delete science from scores object
+//expected output: { math: 350, computer: 701 }
+
+delete scores.science;
+
+console.log(scores); // Output: { math: 350, computer: 701 }
+
+
+// alternative and better methode
+const { science, ...updatedScores } = scores;
+
+console.log(updatedScores); // Output: { math: 350, computer: 701 }
+
+// Q4. add a new subject called 'english' with score 800
+//expected output: { math: 350, science: 503, computer: 701}
+
+
+// Method 1: Add 'english' using dot notation
+scores.english = 800;
+
+console.log(scores); 
+// Output: { math: 350, science: 503, computer: 701, english: 800 }
+
+
+// Method 2: Add 'english' using Object.assign()
+Object.assign(scores, { english: 800 });
+
+console.log(scores); 
+// Output: { math: 350, science: 503, computer: 701, english: 800 }
