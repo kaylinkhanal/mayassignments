6a82c725fbe392ec10f2c7902e1c6bb32f411fec
Jagat05 <joshijagaths@gmail.com>
2025-05-08 08:32:16 +0545

    msg

--- arrays/level 1.js
+++ arrays/level 1.js
@@ -1,11 +1,17 @@
 const arr = ['ram', 'bahadur', 'thapa']
 
 //Q1 output should be 'RAM-BAHADUR-THAPA'
-console.log(arr.join('-').toUpperCase());
+console.log(arr.join('-').toUpperCase());     // Join() method joins all elements of an array into a string 
+                                              // and toUpperCase() method converts the string to uppercase.
 
 
 //Q2 output should be ['ram','thapa']
-arr1=arr[0];
-arr2=arr[2];
 
-console.log(arr1.concat(arr2));
+ // console.log(arr[0].concat(arr[arr.length-1]));  
+ //arr.length-1 is used to get the last element of the array. 
+ // concat() method is used to join two or more strings.
+
+
+ 
+arr.splice(1, 1);   // splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place.
+console.log(arr);
