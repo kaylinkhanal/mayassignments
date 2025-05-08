0ebf33e434d31cc2d43e804b9ddabdc6d6811666
Kaylin Khanal <kaylinkhanal@Kaylins-MacBook-Pro-2.local>
2025-05-08 09:00:20 +0545

    added level1 of objects

--- objects/basic.js
+++ objects/basic.js
@@ -10,16 +10,24 @@ let scores =  {
 }
 
 
-console.log(scores.science)
+
 
 
 //Q1. calculate the percetange scored, full marks of each subject is 1000, and round off to 2 decimal places
+console.log(((scores.math + scores.science + scores.computer)/3000*100).toFixed(2))
 //Q2. what is destructuring an object. solve Q1 by destructuring method.
-const {math, science, a} = scores;
+const {math, science, computer} = scores;
 console.log((math+science+computer)/3000*100)
 
 // Q3. delete science from scores object
 //expected output: { math: 350, computer: 701 }
+delete scores.science
+console.log(scores)
+// const { dance, ...allOther} = scores;   ---- rest operator
+// console.log( allOther)
+// const backup = {...scores}    ---spread operator
 
 // Q4. add a new subject called 'english' with score 800
-//expected output: { math: 350, science: 503, computer: 701}
+//expected output: { math: 350,  computer: 701, english: 800 }
+scores.english = 800
+console.log(scores)
--- objects/level1.js
+++ objects/level1.js
@@ -1,3 +1,15 @@
 //non primitive data types:
+const peopleMap =  {
+    'John':'Thapa',
+    'Jane':'Doe',
+    'Bob':'Smith'
+}
+
+//Q1. convert to array
+//expected output : [['John ','Thapa'],['Jane','Doe'],['Bob','Smith']]
 
 
+// calculate the number of items in an object
+//expected output is 3
+
+// Q3. Print all fullname in this format 'John,Jane,Bob'
