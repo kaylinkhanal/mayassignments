8afdc48079cb3a86ff02af484005252b77e80d55
BhabishyaKC <bhabishyakc19@gmail.com>
2025-05-08 09:35:03 +0545

    level 1 objects

---
+++ objects/level1.js
@@ -0,0 +1,22 @@
+//non primitive data types:
+const peopleMap =  {
+    'John':'Thapa',
+    'Jane':'Doe',
+    'Bob' :'Smith'
+}
+
+//Q1. convert to array
+//expected output : [['John ','Thapa'],['Jane','Doe'],['Bob','Smith']]
+const arr= Object.entries(peopleMap);
+console.log(arr);
+
+// calculate the number of items in an object
+//expected output is 3
+console.log(Object.entries(peopleMap).legth);
+
+
+// Q3. Print all fullname in this format 'John,Jane,Bob'
+console.log(Object.keys(peopleMap).join)
+
+// Q3. Print all fullname in this format 'Thapa,Doe,Smith'
+console.log(Object.values(peopleMap).join)
