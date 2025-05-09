1cc92d1f5a15162cf09591b461e5a417831d70f1
Kaylin Khanal <kaylinkhanal@Kaylins-MacBook-Pro-2.local>
2025-05-09 09:29:53 +0545

    added function basic and looping basic

--- objects/level1.js
+++ objects/level1.js
@@ -7,9 +7,13 @@ const peopleMap =  {
 
 //Q1. convert to array
 //expected output : [['John ','Thapa'],['Jane','Doe'],['Bob','Smith']]
-
+console.log(Object.entries(peopleMap))
 
 // calculate the number of items in an object
 //expected output is 3
+console.log(Object.keys(peopleMap).length)
 
 // Q3. Print all fullname in this format 'John,Jane,Bob'
+console.log(Object.keys(peopleMap).join())
+
+// HW: optional chaining in javascript
