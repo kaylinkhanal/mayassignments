51cc06a930f1239e8618bcad5c59979b77b5b8bc
Kushmita Timsina <khusitimsina15@gmail.com>
2025-05-09 08:16:25 +0545

    arrys problems solved

--- arrays/level 1.js
+++ arrays/level 1.js
@@ -1,6 +1,11 @@
 const arr = ['ram', 'bahadur', 'thapa']
 
 //Q1 output should be 'RAM-BAHADUR-THAPA'
+console.log(toUpperCase()( arr.join('-')));
 
 
 //Q2 output should be ['ram','thapa']
+
+const Arr = [arr[0], arr[2]];
+
+console.log(Arr); 
