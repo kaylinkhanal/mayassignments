24b81459a55c5dca3b981bd4dad9139f62b4be5a
Kushmita Timsina <khusitimsina15@gmail.com>
2025-05-09 14:43:30 +0545

    function level1 task solved

--- objects/level1.js
+++ objects/level1.js
@@ -7,9 +7,15 @@ const peopleMap =  {
 
 //Q1. convert to array
 //expected output : [['John ','Thapa'],['Jane','Doe'],['Bob','Smith']]
-
+const peopleArray = Object.entries(peopleMap);
+console.log(peopleArray);
 
 // calculate the number of items in an object
 //expected output is 3
+const numberOfPeople = Object.keys(peopleMap).length;
+console.log(numberOfPeople); 
+
 
 // Q3. Print all fullname in this format 'John,Jane,Bob'
+const fullNames = Object.keys(peopleMap).join(',');
+console.log(fullNames);
