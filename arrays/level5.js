9f8b44e3e244b56a5d2027c1b6a99332331c6913
Laxmi-999 <regmilaxmi325@gmail.com>
2025-05-12 08:23:52 +0545

    foreach and reduce assignmenet done

--- arrays/level5.js
+++ arrays/level5.js
@@ -5,25 +5,61 @@ let sum  = 0
 
 arr.forEach((item)=>{
     //Q1. write if statement here to only sum numbers greater than 4
+    if(item > 4){
         sum = sum + item
+    }
+    console.log(sum);
+    
 })
 
-console.log(sum)
 
 
 const filteredOutput = arr.filter((item)=>{
-        //Q2 only return odd numbers
-        return item
+    if(item % 2 !== 0){
+     return item;
+    }
+
+    
    
 })
+    console.log("filteredOutput", filteredOutput);
 
-console.log(filteredOutput)
 
 
 
 const mapping = arr.map((item)=>{
- ///write your code here
-    return item
+ 
+    if (item % 2 != 0){
+        return 1;
+    }else{
+        return 0;
+    }
+      
 })
-console.log(mapping)
 //Q3. output should be [1,1,0,1,0]
+console.log("mapping", mapping);
+
+const shoppingCart = [
+  { name: 'Laptop', quantity: 10, price: 1200 },
+  { name: 'Mouse', quantity: 3, price: 25 },
+  { name: 'Keyboard',quantity: 10, price: 75 },
+  { name: 'Monitor',quantity: 3,  price: 300 }
+];
+
+
+// using forEach calculate the total amount user needs to pay
+    let totalAmountForEach = 0; 
+
+shoppingCart.forEach((item) => {
+  totalAmountForEach +=  item.quantity * item.price;
+});
+
+console.log("Total amount using forEach:", totalAmountForEach);
+
+
+// using reduce do the same
+const totalAmountReduce = shoppingCart.reduce((total, item) => {
+    return total + item.quantity * item.price;
+        }, 0); 
+
+console.log("Total amount using reduce:", totalAmountReduce);
