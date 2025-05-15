175542d4c78d2eb1691962da8ddaac613283b0b2
BhabishyaKC <bhabishyakc19@gmail.com>
2025-05-15 15:03:23 +0545

    basic arrays problems

--- arrays/level2.js
+++ arrays/level2.js
@@ -3,3 +3,7 @@ arr = 'ram'
 console.log(arr)
 
 //map vs foreach vs filter 
+
+// ->> Map can transform the Elements
+// ->> forEach displays the wanted or selected element only 
+// ->> filter is used to separate and display the elements that satisfies certain conditons
