1c7bfea9a9f87d2a143efd53afc1aed1f36bfcee
BhabishyaKC <bhabishyakc19@gmail.com>
2025-05-15 20:19:40 +0545

    arrays basic

--- arrays/basic.js
+++ arrays/basic.js
@@ -6,3 +6,9 @@ console.log(arr.length)
 
 // Q2 output should be [1,4,5]
  console.log(arr.slice(0,3))
+
+ // Q3 What is the value of the element at index 3 in arr?
+ console.log(arr[3])
+
+ //Q4 What are the elements of arr starting from index 2 up to (but not including) index 5? 
+ console.log(arr.slice(2,6))
