3d6cab71c6712bd1a4ee825ff43f7209c6b38714
BhabishyaKC <bhabishyakc19@gmail.com>
2025-05-15 09:57:36 +0545

    level3 objects problem solved

--- objects/level3.js
+++ objects/level3.js
@@ -1,1 +1,25 @@
 
+
+const shoppingCart = [
+  { name: 'Laptop', quantity: 10, price: 1200 },
+  { name: 'Mouse', quantity: 3, price: 25 },
+  { name: 'Keyboard',quantity: 10, price: 75 },
+  { name: 'Monitor',quantity: 3,  price: 300 }
+];
+
+
+// using forEach calculate the total amount user needs to pay
+let totalPrice = 0;
+shoppingCart.forEach(item => {
+  totalPrice += item.price * item.quantity;
+});
+console.log(totalPrice)
+
+
+// using reduce do the same
+
+const reducedPrice = shoppingCart.reduce((accumulator, currentItem) => {
+  return accumulator + (currentItem.price * currentItem.quantity);
+}, 0);
+
+console.log(reducedPrice);
