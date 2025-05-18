4d4c0fc4d235bcfff0afb4886cc4f7e3f92d55e2
pratham karki <prathamkarki21@gmail.com>
2025-05-18 21:00:54 +0545

    merge conflict resolved

--- objects/basic.js
+++ objects/basic.js
@@ -10,7 +10,6 @@ let scores =  {
 }
 
 
-<<<<<<< HEAD
 // console.log(scores.kaylin)
 
 
@@ -32,26 +31,3 @@ console.log(scores);
 scores.computer = 701;
 scores.english = 800;
 console.log(scores);
-=======
-
-
-
-//Q1. calculate the percetange scored, full marks of each subject is 1000, and round off to 2 decimal places
-console.log(((scores.math + scores.science + scores.computer)/3000*100).toFixed(2))
-//Q2. what is destructuring an object. solve Q1 by destructuring method.
-const {math, science, computer} = scores;
-console.log((math+science+computer)/3000*100)
-
-// Q3. delete science from scores object
-//expected output: { math: 350, computer: 701 }
-delete scores.science
-console.log(scores)
-// const { dance, ...allOther} = scores;   ---- rest operator
-// console.log( allOther)
-// const backup = {...scores}    ---spread operator
-
-// Q4. add a new subject called 'english' with score 800
-//expected output: { math: 350,  computer: 701, english: 800 }
-scores.english = 800
-console.log(scores)
->>>>>>> 1cc92d1f5a15162cf09591b461e5a417831d70f1
