cbee4fce357904e97b2a68e610f792d0bdcf9bdc
BhabishyaKC <bhabishyakc19@gmail.com>
2025-05-20 12:34:05 +0545

    basic.js updated

--- arrays/basic.js
+++ arrays/basic.js
@@ -1,14 +1,14 @@
-const arr = [1,4,5,6,7,1]
+ const arr = [1,4,5,6,7,1]
 
-// Q1 output should be 6
+// // // Q1 output should be 6
 
-console.log(arr.length)
+ console.log(arr.length)
 
-// Q2 output should be [1,4,5]
- console.log(arr.slice(0,3))
+// // // Q2 output should be [1,4,5]
+  console.log(arr.slice(0,3))
 
- // Q3 What is the value of the element at index 3 in arr?
- console.log(arr[3])
+/  // Q3 What is the value of the element at index 3 in arr?
+  console.log(arr[3])
 
- //Q4 What are the elements of arr starting from index 2 up to (but not including) index 5? 
- console.log(arr.slice(2,6))
+  //Q4 What are the elements of arr starting from index 2 up to (but not including) index 5? 
+  console.log(arr.slice(2,6))
